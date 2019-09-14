53ff562419ff816e33aaff0cfc3a64200d3052f3
Juan <j.juancamilojurado@gmail.com>
2019-09-13 23:26:10 -0500

    Initial commit

---
+++ Taller4.4/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Taller4._4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtmultiplo = new System.Windows.Forms.TextBox();
+            this.btnlistar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.txtdesde = new System.Windows.Forms.TextBox();
+            this.txthasta = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(92, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Multiplos de x";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(85, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Listar multiplos de :";
+            // 
+            // txtmultiplo
+            // 
+            this.txtmultiplo.Location = new System.Drawing.Point(32, 94);
+            this.txtmultiplo.Name = "txtmultiplo";
+            this.txtmultiplo.Size = new System.Drawing.Size(265, 20);
+            this.txtmultiplo.TabIndex = 2;
+            // 
+            // btnlistar
+            // 
+            this.btnlistar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlistar.Location = new System.Drawing.Point(112, 439);
+            this.btnlistar.Name = "btnlistar";
+            this.btnlistar.Size = new System.Drawing.Size(94, 27);
+            this.btnlistar.TabIndex = 3;
+            this.btnlistar.Text = "Listar.";
+            this.btnlistar.UseVisualStyleBackColor = true;
+            this.btnlistar.Click += new System.EventHandler(this.Btnlistar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Desde :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(236, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Hasta:";
+            // 
+            // lista
+            // 
+            this.lista.FormattingEnabled = true;
+            this.lista.Location = new System.Drawing.Point(32, 172);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(265, 251);
+            this.lista.TabIndex = 8;
+            // 
+            // txtdesde
+            // 
+            this.txtdesde.Location = new System.Drawing.Point(32, 146);
+            this.txtdesde.Name = "txtdesde";
+            this.txtdesde.Size = new System.Drawing.Size(93, 20);
+            this.txtdesde.TabIndex = 11;
+            // 
+            // txthasta
+            // 
+            this.txthasta.Location = new System.Drawing.Point(204, 146);
+            this.txthasta.Name = "txthasta";
+            this.txthasta.Size = new System.Drawing.Size(93, 20);
+            this.txthasta.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 487);
+            this.Controls.Add(this.txthasta);
+            this.Controls.Add(this.txtdesde);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnlistar);
+            this.Controls.Add(this.txtmultiplo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtmultiplo;
+        private System.Windows.Forms.Button btnlistar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.TextBox txtdesde;
+        private System.Windows.Forms.TextBox txthasta;
+    }
+}
+
---
+++ Taller4.4/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taller4._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btnlistar_Click(object sender, EventArgs e)
+        {
+            int desde = int.Parse(txtdesde.Text);
+            int hasta = int.Parse(txthasta.Text);
+            int multiplo = int.Parse(txtmultiplo.Text);
+
+            lista.Items.Clear();
+
+            for (int i = desde; i <= hasta; i++ )
+            {
+
+                if (i % multiplo == 0)
+                {
+                    lista.Items.Add(i);
+
+                }
+                      
+            }
+
+
+
+        }
+    }
+}
